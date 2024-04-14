153b35e60e46172dfb98abb38240a50bdf1a6bb3
jerebjo <148748911+jerebjo@users.noreply.github.com>
2024-04-14 17:39:34 +0300

    add ja delete lisäys

--- demo/src/main/java/com/example/demo/web/PokeController.java
+++ demo/src/main/java/com/example/demo/web/PokeController.java
@@ -3,9 +3,12 @@ package com.example.demo.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.demo.model.PokeRepository;
+import com.example.demo.model.Pokemon;
 
 @Controller
 public class PokeController {
@@ -13,9 +16,27 @@ public class PokeController {
     @Autowired
     private PokeRepository repository;
 
-    @RequestMapping("/pokemonlist")
+    @RequestMapping(value = { "/", "/pokemonlist" })
     public String pokemonList(Model model) {
         model.addAttribute("pokemons", repository.findAll());
-        return "pokemonlist"; // This should be the name of your Thymeleaf template file
+        return "pokemonlist";
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deletePokemon(@PathVariable Long id, Model model) {
+        repository.deleteById(id);
+        return "redirect:../pokemonlist";
+    }
+
+    @RequestMapping(value = "/add")
+    public String addPokemon(Model model) {
+        model.addAttribute("pokemon", new Pokemon());
+        return "addpokemon";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Pokemon pokemon) {
+        repository.save(pokemon);
+        return "redirect:pokemonlist";
     }
 }
